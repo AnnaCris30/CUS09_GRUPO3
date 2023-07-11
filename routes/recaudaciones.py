cd16b69aed4c502ddf1ec2df7f046ae5c476d05b
AnnaCris30 <anacristina.huaman@unmsm.edu.pe>
2023-07-11 00:06:04 -0500

    .

--- routes/recaudaciones.py
+++ routes/recaudaciones.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template as rt, request, url_for, redirect
 bp = Blueprint('recaudaciones', __name__)
 
 @bp.route('/', methods=['POST','GET'])
-def cotizaciones():
+def recaudaciones():
 
 
 

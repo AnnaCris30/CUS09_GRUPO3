3b638efa760d24a8a256a01799502594ea1ce28b
AnnaCris30 <anacristina.huaman@unmsm.edu.pe>
2023-07-11 00:00:38 -0500

    .

--- models/recaudacion.py
+++ models/recaudacion.py
@@ -4,10 +4,10 @@ class Recaudacion(db.Model):
     fecha_operacion= db.Column(db.Date)
     importe = db.Column(db.Double)
     id_tipo_moneda = db.Column(db.Integer, db.ForeignKey('tipo_moneda.id_tipo_moneda'))
-    id_recaudacion_estado = db.Column(db.String(150), db.Foreignkey('recaudacion_estado.id_recaudacion_estado'))
+    id_recaudacion_estado = db.Column(db.String(150), db.ForeignKey('recaudacion_estado.id_recaudacion_estado'))
 
     tipo_moneda = db.relationship('TipoMoneda', backref='recaudacion')
-    recaudacion_estado = db.relationship('TipoMoneda', backref='recaudacion')
+    recaudacion_estado = db.relationship('EstadoRecaudacion', backref='recaudacion')
     
     def __init__(self,fecha_operacion, id_tipo_moneda, importe,id_recaudacion_estado):
         self.fecha_operacion= fecha_operacion

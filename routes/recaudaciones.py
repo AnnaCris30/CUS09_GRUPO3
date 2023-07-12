25969d9f81d1b6b297e5fac82642c65edb0311e5
AnnaCris30 <anacristina.huaman@unmsm.edu.pe>
2023-07-11 23:12:30 -0500

    def buscar numDocumento

--- routes/recaudaciones.py
+++ routes/recaudaciones.py
@@ -1,10 +1,44 @@
 from flask import Blueprint, render_template as rt, request, url_for, redirect
-
+from models.persona import Persona
+from flask import jsonify
 bp = Blueprint('recaudaciones', __name__)
 
 @bp.route('/', methods=['POST','GET'])
 def recaudaciones():
 
-
-
     return rt("recaudacion.html")
+ 
+    
+
+@bp.route('/buscar', methods=['POST'])
+def buscar_numDocumento():
+    num_documento = request.form['num_documento']
+
+    # Lógica para buscar el número de documento en la base de datos y obtener los valores correspondientes
+    persona = Persona.query.filter_by(ndocumento=num_documento).first()
+
+    if persona:
+        # Si se encuentra el número de documento, obtener los valores correspondientes
+        nombres = persona.nombres
+        banco = persona.banco
+        moneda = persona.moneda
+        nro_cuenta = persona.nro_cuenta
+        nro_cuenta_predio = persona.nro_cuenta_predio
+        tipo_autorizacion = persona.tipo_autorizacion
+        estado = persona.estado
+
+        # Devolver la respuesta al cliente en formato JSON con los valores obtenidos
+        response = {
+            'success': True,
+            'data': {
+                'nombres': nombres, 
+            }
+        }
+        return jsonify(response)
+    else:
+        # Si no se encuentra el número de documento, devolver un mensaje de error al cliente
+        response = {
+            'success': False,
+            'message': 'Número de documento no encontrado'
+        }
+        return jsonify(response)

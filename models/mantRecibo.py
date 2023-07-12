f059f0dcfe9375a945ace8d589ac683fc393c30a
Maricieloaf02 <maricielo.af0@gmail.com>
2023-07-11 23:41:53 -0500

    mantRecibo

--- models/mantRecibo.py
+++ models/mantRecibo.py
@@ -8,19 +8,22 @@ class MantRecibo(db.Model):
     fecha_emision = db.Column(db.Date)
     fecha_vencimiento = db.Column(db.Date)
     importe = db.Column(db.DECIMAL)
-    ajuste = db.Column(db.DECIMAL(6, 2))
+    ajuste = db.Column(db.numeric(6,2))
     observacion = db.Column(db.String(100))
     id_recibo_estado = db.Column(db.Integer, db.ForeignKey('recibo_estado.id_recibo_estado'))
 
     casa = db.relationship('Casa', backref='mantRecibo')
     recibo_estado = db.relationship('ReciboEstado', backref='mantRecibo')
 
-    def __init__(self, id_casa, id_predio, id_estado, numero, piso,
-                 area,participacion):
+    def __init__(self, id_mant_recibo, id_casa, periodo, n_recibo, fecha_emision,
+                 fecha_vencimiento,importe, ajuste, observacion, id_recibo_estado):
+        self.id_mant_recibo = id_mant_recibo
         self.id_casa = id_casa
-        self.id_predio = id_predio
-        self.id_estado = id_estado
-        self.numero = numero
-        self.piso = piso
-        self.area = area
-        self.participacion = participacion
+        self.periodo = periodo
+        self.n_recibo = n_recibo
+        self.fecha_emision = fecha_emision
+        self.fecha_vencimiento = fecha_vencimiento
+        self.importe = importe
+        self.ajuste = ajuste
+        self.observacion = observacion
+        self.id_recibo_estado = id_recibo_estado

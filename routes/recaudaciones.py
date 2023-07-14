e3fb47bdef0233c56517aed7c62fedeec4899460
Maricieloaf02 <maricielo.af0@gmail.com>
2023-07-14 00:47:24 -0500

    mejoras-

--- routes/recaudaciones.py
+++ routes/recaudaciones.py
@@ -4,23 +4,28 @@ from models.persona import Persona
 from models.cuenta import Cuenta
 from models.recaudacion import Recaudacion
 from schemas.recaudacionSchema import RecaudacionSchema
-
-
+from models.mantRecibo import MantRecibo
 from flask import jsonify
+from models.mantRecibo import MantRecibo
+from schemas.mantReciboSchema import MantReciboSchema
+
+
 bp = Blueprint('recaudaciones', __name__)
 
 @bp.route('/', methods=['POST','GET'])
 def recaudaciones():
     #Obtener todas las recaudaciones
     recaudaciones= Recaudacion.query.all()
-    
+    recibos = MantRecibo.query.all()
     # Serializar los objetos por medio de schemas
     recaudaciones_serializadas=RecaudacionSchema(many=True).dump(recaudaciones)
-
-    return rt("recaudacion.html",recaudaciones=recaudaciones_serializadas)
- 
+    recibos_serializados=MantReciboSchema(many=True).dump(recibos)
     
-
+    return rt("recaudacion.html",recaudaciones=recaudaciones_serializadas, 
+        recibos=recibos_serializados)
+    
+ 
+## ------------BUSCAR POR NUMERO DE DOCUMENTO---------------
 @bp.route('/buscar', methods=['POST'])
 def buscar_numDocumento():
     num_documento = request.form['num_documento']
@@ -32,7 +37,7 @@ def buscar_numDocumento():
         # Si se encuentra el número de documento, obtener los valores correspondientes        
         nombres = persona.nombres
         id_persona=persona.id_persona
-        cuenta = Cuenta.query.filter_by(id_persona=id_persona).first()
+        cuenta = Cuenta.query.filter_by(id_persona=id_persona).first()  ##como la llave foranea
         banco=cuenta.banco.descripcion
         moneda = cuenta.tipo_moneda.descripcion
         ncuenta = cuenta.ncuenta
@@ -53,3 +58,35 @@ def buscar_numDocumento():
         # Si no se encuentra el número de documento, redirigir a la página inicial con un mensaje de error
         flash('Número de documento no encontrado', 'error')
         return redirect(url_for('recaudaciones'))
+
+
+
+##------------BUSCAR POR NUMERO RECIBO---------------
+@bp.route('/buscarRecibo', methods=['POST'])
+def buscar_nroRecibo():
+    num_recibo = request.form['num_recibo']
+
+    # Lógica para buscar el número de recibo en la base de datos y obtener los valores correspondientes
+    mantRecibo = MantRecibo.query.filter_by(n_recibo=num_recibo).first()
+    
+    if mantRecibo:
+        # Si se encuentra el número de documento, obtener los valores correspondientes        
+        recibo_estado = mantRecibo.recibo_estado.descripcion
+        importe = mantRecibo.importe
+        
+        
+        # Devolver la respuesta al cliente en formato JSON con los valores obtenidos
+        response = {
+            'success': True,
+            'data': {
+                'recibo_estado': recibo_estado,
+                'importe' : importe
+            }
+        }
+        return jsonify(response)
+    else:
+         
+        # Si no se encuentra el número de documento, redirigir a la página inicial con un mensaje de error
+        flash('Número de recibo no encontrado', 'error')
+        return redirect(url_for('recaudaciones'))
+      
